ffaae996b8dca6f417b47bcd9290a60b4bc881c2
mohdfaizmaik <49128205+mohdfaizmaik@users.noreply.github.com>
2021-07-26 23:29:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function insert(num){
-    var text = document.form.textinput.value
-    document.form.textinput.value = text + num
+    var fz = document.form.textinput.value
+    document.form.textinput.value = fz + num
 }
 
 function calculate(){
-    var text =document.form.textinput.value
-    document.form.textinput.value = eval(text)
+    var fz =document.form.textinput.value
+    document.form.textinput.value = eval(fz)
 }
 
 function clean(){
@@ -13,11 +13,47 @@ function clean(){
 }
 
 function back(){
-    var text =document.form.textinput.value
-    document.form.textinput.value = text.substring(0,text.length-1)
+    var fz =document.form.textinput.value
+    document.form.textinput.value = fz.substring(0,fz.length-1)
 }
 
 function sqrt() {
-    var text = document.form.textinput.value
-    document.form.textinput.value = Math.sqrt(text)
+    var fz = document.form.textinput.value
+    document.form.textinput.value = Math.sqrt(fz)
+}
+
+function calc_log(){
+    var fz = document.form.textinput.value
+    document.form.textinput.value = Math.log(fz)
+}
+
+function exp(){
+    var fz = document.form.textinput.value
+    document.form.textinput.value = Math.exp(fz)
+}
+
+function percent(){
+    var fz = document.form.textinput.value
+    document.form.textinput.value = fz/100
+}
+
+
+function radian(){
+    var fz = document.form.textinput.value
+    document.form.textinput.value = parseFloat((fz * (3.14/180)).toFixed(3))
+}
+
+function degree(){
+    var fz = document.form.textinput.value
+    document.form.textinput.value = parseFloat((fz * (180 / 3.14)).toFixed(2))
+}
+
+function cos(){
+    var fz = document.form.textinput.value
+    document.form.textinput.value = parseFloat(Math.cos(fz * (Math.PI / 180)).toFixed(2))
+}
+
+function sin(){
+    var fz = document.form.textinput.value
+    document.form.textinput.value = parseFloat(Math.sin(fz * (Math.PI / 180)).toFixed(2))
 }
